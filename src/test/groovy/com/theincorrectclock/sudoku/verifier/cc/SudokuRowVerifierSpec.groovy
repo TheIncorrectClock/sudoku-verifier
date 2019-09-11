89196cdd504397df711bfbfc1821fe2e249f7044
Bartosz Wiśniewski <theincorrectclock@gmail.com>
2019-09-11 20:53:21 +0100

    added more clean code row verifier tests

--- src/test/groovy/com/theincorrectclock/sudoku/verifier/cc/SudokuRowVerifierSpec.groovy
+++ src/test/groovy/com/theincorrectclock/sudoku/verifier/cc/SudokuRowVerifierSpec.groovy
@@ -1,16 +1,45 @@
-package com.theincorrectclock.sudoku.verifier
+package com.theincorrectclock.sudoku.verifier.cc
 
 import spock.lang.Specification
 
 class SudokuRowVerifierSpec extends Specification {
 
-    def "verifies valid sudoku row as valid"() {
+    def "verifies empty sudoku row as valid"() {
+        given:
+        SudokuRowVerifier rowVerifier = new SudokuRowVerifier()
+        and:
+        int[] row = [-1, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+        when:
+        boolean valid = rowVerifier.verify(row)
+        
         then:
-        false
+        true == valid
+    }
+
+    def "verifies valid sudoku row as valid"() {
+        given:
+        SudokuRowVerifier rowVerifier = new SudokuRowVerifier()
+        and:
+        int[] row = [-1, 1, 0, 0, 1, 1, 1, 0, 0, 0]
+
+        when:
+        boolean valid = rowVerifier.verify(row)
+
+        then:
+        true == valid
     }
 
     def "verifies invalid sudoku row as invalid"() {
+        given:
+        SudokuRowVerifier rowVerifier = new SudokuRowVerifier()
+        and:
+        int[] row = [-1, 1, 0, 0, 2, 0, 1, 0, 0, 0]
+
+        when:
+        boolean valid = rowVerifier.verify(row)
+
         then:
-        false
+        false == valid
     }
 }
