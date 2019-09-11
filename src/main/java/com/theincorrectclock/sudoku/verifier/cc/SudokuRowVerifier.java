c19979b75662e49211ddf846308f689d4ded2162
Bartosz Wiśniewski <theincorrectclock@gmail.com>
2019-09-11 21:25:33 +0100

    switched surefire to 2.20; added implementation O(n) to row verifier

--- src/main/java/com/theincorrectclock/sudoku/verifier/cc/SudokuRowVerifier.java
+++ src/main/java/com/theincorrectclock/sudoku/verifier/cc/SudokuRowVerifier.java
@@ -1,8 +1,16 @@
 package com.theincorrectclock.sudoku.verifier.cc;
 
-public class SudokuRowVerifier {
+class SudokuRowVerifier {
+
+    private static final int FIRST_ELEMENT = 1;
+    private static final int LAST_ELEMENT = 10;
 
     boolean verify(int[] row) {
-        return false;
+        for (int i = FIRST_ELEMENT; i < LAST_ELEMENT; i++) {
+            if (row[i] > 1) {
+                return false;
+            }
+        }
+        return true;
     }
 }
